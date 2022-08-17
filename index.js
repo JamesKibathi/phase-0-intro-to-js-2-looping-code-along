877ea406837a1bf48982fcb1316bcc3026b9fb25
James Njenga <knjenga94@gmail.com>
2022-08-17 20:28:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Code your solutions in this file
+/*
+for(let age=30;age<40;age++){
+console.log(`I'm ${age} years old. Happy birthday to me!`);
+
+}*/
+
+/*
+const gifts=["teddy bear","drone","doll"];
+
+function wrapGifts(gifts){
+    for(let i=0;i<gifts.length;i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+      
+    }
+return gifts;
+}
+wrapGifts(gifts); */
+
+/*
+// This is a for Loop trial- Thank You Messages
+
+const messages=[];
+function writeCards(messages,myEvent){
+
+    for(let i=0;i<messages.length;i++){
+let thankMsg=`Thank you,${messages[i]},for the wonderful,${myEvent} gift!`;
+console.log(thankMsg);
+
+
+}
+    return messages;
+}
+
+writeCards(["Ada","Brendan","Ali"],"birthday");
+*/
+
+// While loop assignment
+
+function countDown(x){
+    let count=x;
+    while(count>=0){
+        console.log(count--);
+    }
+}
+countDown(10);
